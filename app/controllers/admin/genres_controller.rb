fd8ed0cf9326ad26d3018905eeaa8bcec3baf9c9
vagrant <vagrant@localhost.localdomain>
2020-11-04 13:44:53 +0000

    genre-checkbox

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -10,14 +10,11 @@ class Admin::GenresController < ApplicationController
 	end
 	def edit
 		@genre = Genre.find(params[:id])
-		redirect_to admin_genres_path
 	end
 	def update
-		if @genre.update(genre_params)
-			redirect to admin_genres_path
-		else
-			render :edit
-		end
+        genre = Genre.find(params[:id])
+		genre.update(genre_params)
+		redirect_to admin_genres_path
 	end
 	private
     def genre_params
