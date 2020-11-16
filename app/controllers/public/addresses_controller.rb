9b22b2d595b01a2ac1202860dec5ba4ae97a681e
vagrant <vagrant@localhost.localdomain>
2020-11-16 14:48:50 +0000

    address

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,24 @@
 class Public::AddressesController < ApplicationController
+	def index
+		@addresses = Address.all
+		@address = Address.new
+	end
+	def edit
+	end
+	def create
+		@address = Address.new(address_params)
+		@address.save
+		redirect_to addresses_path
+	end
+	def update
+		@address = Address.find(params[:id])
+		@address.update(address_params)
+		redirect_to addresses_path
+	end
+	def destroy
+	end
+	private
+	def address_params
+		params.require(:address).permit(:customer_id, :name, :postal_code, :address)
+	end
 end
